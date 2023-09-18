fbd3739834e4339a9c518b3d015cd39146b2ea30
Warren Zhu <warren.zhu25@gmail.com>
2023-09-18 12:51:41 -0700

    Polish text table output to left aligned and pad with space

--- src/main/scala/org/apache/spark/insight/util/Tabulator.scala
+++ src/main/scala/org/apache/spark/insight/util/Tabulator.scala
@@ -23,12 +23,12 @@ object Tabulator {
   private def formatRow(row: Seq[Any], colSizes: Seq[Int]): String = {
     val cells =
       for ((item, size) <- row.zip(colSizes))
-        yield if (size == 0) "" else ("%" + size + "s").format(item)
-    cells.mkString("|", "|", "|")
+        yield if (size == 0) "" else (" %1$-" + size + "s ").format(item)
+    cells.mkString("||", "|", "|")
   }
 
   private def rowSeparator(colSizes: Seq[Int]): String =
-    colSizes map {
-      "-" * _
+    colSizes map { col =>
+      "-" * (col + 2)
     } mkString("+", "+", "+")
 }
