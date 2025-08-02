b6ca07279ea1bd2944d1a149a6eb54db44e00c12
Warren Zhu <warren.zhu25@gmail.com>
2025-08-02 08:53:27 -0700

    feat: Add web server mode with http4s

--- src/main/scala/com/microsoft/spark/insight/server/WebServer.scala
+++ src/main/scala/com/microsoft/spark/insight/server/WebServer.scala
@@ -5,11 +5,13 @@ import cats.effect.unsafe.implicits.global
 import org.apache.spark.insight.analyzer.HtmlReportAnalyzer
 import org.http4s.HttpRoutes
 import org.http4s.dsl.io._
-import org.http4s.scalatags._
+import org.http4s.headers.`Content-Type`
+import org.http4s.{Header, HttpRoutes, MediaType}
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.implicits._
 import org.apache.spark.insight.fetcher.SparkFetcher
-import scalatags.Text.all._
+import org.typelevel.ci.CIStringSyntax
+import cats.implicits._
 
 object WebServer {
 
@@ -26,20 +28,24 @@ object WebServer {
   val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
     case GET -> Root =>
       Ok(
-        html(
-          body(
-            h1("Spark Insight"),
-            form(action := "/analyze", method := "post")(
-              label(`for` := "url1")("Application URL 1:"),
-              input(tpe := "text", id := "url1", name := "url1", required),
-              br,
-              label(`for` := "url2")("Application URL 2 (optional):"),
-              input(tpe := "text", id := "url2", name := "url2"),
-              br,
-              input(tpe := "submit", value := "Analyze")
-            )
-          )
-        )
+        """
+          |<!DOCTYPE html>
+          |<html>
+          |<head>
+          |    <title>Spark Insight</title>
+          |</head>
+          |<body>
+          |<h1>Spark Insight</h1>
+          |<form action="/analyze" method="post">
+          |    <label for="url1">Application URL 1:</label>
+          |    <input type="text" id="url1" name="url1" required><br>
+          |    <label for="url2">Application URL 2 (optional):</label>
+          |    <input type="text" id="url2" name="url2"><br>
+          |    <input type="submit" value="Analyze">
+          |</form>
+          |</body>
+          |</html>
+          |""".stripMargin, Header.Raw("Content-Type".ci, "text/html")
       )
     case req @ POST -> Root / "analyze" =>
       req.decode[org.http4s.UrlForm] { data =>
@@ -51,11 +57,11 @@ object WebServer {
             val appData1 = SparkFetcher.fetchData(url1)
             val appData2 = SparkFetcher.fetchData(url2)
             val result = HtmlReportAnalyzer.analysis(appData1)
-            Ok(result.rows.head.head)
+            Ok(result.rows.head.head, Header.Raw("Content-Type".ci, "text/html"))
           case (Some(url1), None) =>
             val appData = SparkFetcher.fetchData(url1)
             val result = HtmlReportAnalyzer.analysis(appData)
-            Ok(result.rows.head.head)
+            Ok(result.rows.head.head, Header.Raw("Content-Type".ci, "text/html"))
           case _ =>
             BadRequest("Please provide at least one URL.")
         }
