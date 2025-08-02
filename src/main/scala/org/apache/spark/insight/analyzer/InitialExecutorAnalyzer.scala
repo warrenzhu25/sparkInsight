12b04549b28f4f26136105d8f49b81127894125c
Warren Zhu <warren.zhu25@gmail.com>
2025-08-01 23:30:48 -0700

    feat(analyzer): Refine InitialExecutorAnalyzer logic
    Refines the calculation for suggested initial executors in InitialExecutorAnalyzer.

--- src/main/scala/org/apache/spark/insight/analyzer/InitialExecutorAnalyzer.scala
+++ src/main/scala/org/apache/spark/insight/analyzer/InitialExecutorAnalyzer.scala
@@ -3,9 +3,11 @@ package org.apache.spark.insight.analyzer
 import org.apache.spark.insight.fetcher.SparkApplicationData
 
 import java.util.Date
+import scala.concurrent.duration.DurationInt
 
 object InitialExecutorAnalyzer extends Analyzer {
   private val headers = Seq("Config", "Current", "Suggest")
+  private val TARGET_DURATION = 2.minutes
 
   override def analysis(sparkAppData: SparkApplicationData): AnalysisResult = {
     val stageData = sparkAppData.stageData
@@ -18,10 +20,11 @@ object InitialExecutorAnalyzer extends Analyzer {
         s.completionTime.nonEmpty &&
         s.completionTime.get.after(time))
 
-    stages.foreach(s => println(s"${s.stageId}, ${s.executorRunTime}, ${s.executorRunTime / (1000 * 60 * 3)}"))
+    stages.foreach(s => println(s"${s.stageId}, ${s.executorRunTime}, ${s.executorRunTime / (TARGET_DURATION.toMillis)}"))
 
-    val suggested = stages.map(s => s.executorRunTime / (1000 * 60 * 3)).sum.toString
+    val suggested = stages.map(s => math.min(s.executorRunTime / TARGET_DURATION.toMillis, s.numTasks) / 4)
+      .sum
 
-    AnalysisResult("Initial Executor", headers, Seq(Seq("initialExecutors", "2", suggested)))
+    AnalysisResult("Initial Executor", headers, Seq(Seq("initialExecutors", "2", suggested.toString)))
   }
 }
