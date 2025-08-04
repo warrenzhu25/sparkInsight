7f828eec593ad353fbb1e9fd5b2f72d179210147
Warren Zhu <warren.zhu25@gmail.com>
2025-08-03 23:12:40 -0700

    feat: Add smart filter to config diff analyzer

--- src/main/scala/org/apache/spark/insight/analyzer/ConfigDiffAnalyzer.scala
+++ src/main/scala/org/apache/spark/insight/analyzer/ConfigDiffAnalyzer.scala
@@ -1,4 +1,3 @@
-
 package org.apache.spark.insight.analyzer
 
 import org.apache.spark.insight.fetcher.SparkApplicationData
@@ -7,6 +6,20 @@ import org.apache.spark.insight.fetcher.SparkApplicationData
  * An analyzer that compares the Spark configurations of two applications.
  */
 object ConfigDiffAnalyzer extends Analyzer {
+
+  private val USEFUL_CONFIG_PREFIXES = Set(
+    "spark.executor",
+    "spark.driver",
+    "spark.sql",
+    "spark.memory",
+    "spark.storage",
+    "spark.shuffle",
+    "spark.network",
+    "spark.ui",
+    "spark.dynamicAllocation",
+    "spark.serializer",
+    "spark.kryo"
+  )
 
   override def analysis(data1: SparkApplicationData, data2: SparkApplicationData): AnalysisResult = {
     val conf1 = data1.appConf
@@ -18,10 +31,13 @@ object ConfigDiffAnalyzer extends Analyzer {
       val value1 = conf1.get(key)
       val value2 = conf2.get(key)
 
-      if (value1 == value2) {
-        None
-      } else {
+      val isUseful = USEFUL_CONFIG_PREFIXES.exists(prefix => key.startsWith(prefix))
+      val isNewOrRemoved = value1.isEmpty || value2.isEmpty
+
+      if (value1 != value2 && (isUseful || isNewOrRemoved)) {
         Some(Seq(key, value1.getOrElse(""), value2.getOrElse("")))
+      } else {
+        None
       }
     }
 
