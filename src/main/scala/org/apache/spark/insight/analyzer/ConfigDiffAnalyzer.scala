c70bea6f246b4848eeafd844e31924106baaa7f8
Warren Zhu <warren.zhu25@gmail.com>
2025-08-03 23:19:04 -0700

    feat: Improve smart filter for config diff analyzer

--- src/main/scala/org/apache/spark/insight/analyzer/ConfigDiffAnalyzer.scala
+++ src/main/scala/org/apache/spark/insight/analyzer/ConfigDiffAnalyzer.scala
@@ -1,7 +1,8 @@
-
 package org.apache.spark.insight.analyzer
 
 import org.apache.spark.insight.fetcher.SparkApplicationData
+
+import scala.util.matching.Regex
 
 /**
  * An analyzer that compares the Spark configurations of two applications.
@@ -20,6 +21,24 @@ object ConfigDiffAnalyzer extends Analyzer {
     "Serialization" -> Set("spark.serializer", "spark.kryo")
   )
 
+  private val RANDOM_PATTERNS: Seq[Regex] = Seq(
+    "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}".r, // UUID
+    "app-[0-9]{12}-[0-9]{4}".r, // Spark Application ID
+    "job-[0-9]{12}-[0-9]{4}".r, // Spark Job ID
+    "stage-[0-9]{12}-[0-9]{4}".r, // Spark Stage ID
+    "attempt-[0-9]{12}-[0-9]{4}".r, // Spark Attempt ID
+    "task-[0-9]{12}-[0-9]{4}".r, // Spark Task ID
+    "executor-[0-9]{12}-[0-9]{4}".r, // Spark Executor ID
+    "rdd_[0-9]+_[0-9]+".r, // RDD ID
+    "temp_[0-9a-f]{32}".r, // Temporary file/directory
+    "_[0-9a-f]{32}".r, // Suffix with long hex string
+    "[0-9a-f]{32}".r // Long hex string
+  )
+
+  private def isRandom(s: String): Boolean = {
+    RANDOM_PATTERNS.exists(_.findFirstIn(s).isDefined)
+  }
+
   override def analysis(data1: SparkApplicationData, data2: SparkApplicationData): AnalysisResult = {
     val conf1 = data1.appConf
     val conf2 = data2.appConf
@@ -31,10 +50,14 @@ object ConfigDiffAnalyzer extends Analyzer {
       val value2 = conf2.get(key)
 
       if (value1 != value2) {
-        val category = CATEGORIZED_PREFIXES.find { case (_, prefixes) =>
-          prefixes.exists(prefix => key.startsWith(prefix))
-        }.map(_._1).getOrElse("Other")
-        Some((category, key, value1.getOrElse(""), value2.getOrElse("")))
+        if (value1.isDefined && value2.isDefined && isRandom(value1.get) && isRandom(value2.get)) {
+          None
+        } else {
+          val category = CATEGORIZED_PREFIXES.find { case (_, prefixes) =>
+            prefixes.exists(prefix => key.startsWith(prefix))
+          }.map(_._1).getOrElse("Other")
+          Some((category, key, value1.getOrElse(""), value2.getOrElse("")))
+        }
       } else {
         None
       }
